bcde6ba1f18f5cbfcb796ddb954b3df4cd8089fa
Kevin Stradtman <stradtkt22@gmail.com>
2020-11-11 20:38:27 -0500

    Added some of the frontend

--- 02-backend/springbootecommerce/src/main/java/com/stradtkt/springbootecommerce/dao/ProductRepository.java
+++ 02-backend/springbootecommerce/src/main/java/com/stradtkt/springbootecommerce/dao/ProductRepository.java
@@ -3,5 +3,7 @@ package com.stradtkt.springbootecommerce.dao;
 
 import com.stradtkt.springbootecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {}
