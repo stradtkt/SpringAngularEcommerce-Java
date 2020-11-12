0744f40044eb1e1f63a18faf5456e1cfd3d505bf
Kevin Stradtman <stradtkt22@gmail.com>
2020-11-12 11:36:41 -0500

    Search for id both on frontend and backend

--- 02-backend/springbootecommerce/src/main/java/com/stradtkt/springbootecommerce/dao/ProductRepository.java
+++ 02-backend/springbootecommerce/src/main/java/com/stradtkt/springbootecommerce/dao/ProductRepository.java
@@ -2,8 +2,14 @@ package com.stradtkt.springbootecommerce.dao;
 
 
 import com.stradtkt.springbootecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @CrossOrigin("http://localhost:4200")
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+}
